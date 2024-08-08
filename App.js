9a6d84250a4ccda28ebf265bad0430e12cd5e926
malkio <coding.malkio@gmail.com>
2024-08-08 16:19:22 +0800

    ios|android: install react-native-maps, lock expo sdk to version 50 to resolve compatibility issue with google maps

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { AnimatedRegion, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
+
+  const defaultRegion = {
+    latitude: 7.086313,
+    latitudeDelta: 0.122311,
+    longitude: 125.612855,
+    longitudeDelta: 0.066947
+  }
+
+  const markers = [
+    { id: 1, coordinate: { latitude: 7.086313, longitude: 125.612855 }, title: 'Davao City', description: 'City of Royalties' },
+    { id: 2, coordinate: { latitude: 7.0667509868259515, longitude: 125.59637573846157 }, title: 'Davao City', description: 'City of Royalties' },
+    { id: 3, coordinate: { latitude: 7.065347826608068, longitude: 125.60200927325728 }, title: 'Davao City', description: 'City of Royalties' },
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        initialRegion={defaultRegion}
+        onRegionChange={(region) => {
+          console.log(region)
+        }}
+        provider={"google"}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        showsMyLocationButton={true}
+        style={styles.map}
+      >
+        { markers.map((marker,i) => <Marker key={marker.id} coordinate={marker.coordinate} />)}
+      </MapView>
     </View>
   );
 }
@@ -14,7 +40,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
+
